dbe56f516988830866ebf51c511613da32d2eccd
ygrenzinger <yannick.grenzinger@gmail.com>
2017-01-05 12:30:35 +0100

    WiP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFire, AuthProviders } from 'angularfire2';
+import * as _ from "lodash";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  user = null;
+
+  constructor(
+    public af: AngularFire
+  ) {
+    this.af.auth.subscribe(user => {
+      if(user) {
+        this.user = {
+            'email':user.google.email,
+            'name':user.google.displayName
+          };
+      }
+      else {
+        this.user = null;
+      }
+    });
+  }
+
+  login() {
+    this.af.auth.login({
+      provider: AuthProviders.Google
+    });
+  }
+
+  logout() {
+    this.af.auth.logout();
+  }
+
+  isLoggedIn() {
+    return this.user != null;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {
+  AngularFireModule,
+  AuthMethods,
+  AuthProviders
+} from "angularfire2";
 
 import { AppComponent } from './app.component';
+import { MaterialComponent } from './material/material.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCxzgEd7ecIVSILa51C2sXRTHbrmx5m9wc",
+  authDomain: "absainte-crm-7c5a0.firebaseapp.com",
+  databaseURL: "https://absainte-crm-7c5a0.firebaseio.com",
+  storageBucket: "absainte-crm-7c5a0.appspot.com",
+  messagingSenderId: "72292104963"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MaterialComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgbModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig,{
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
